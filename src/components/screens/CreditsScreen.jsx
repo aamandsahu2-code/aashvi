3b5ce0326fc5b6549cc6ab8687c6018a08d3b9ff
aamandsahu2-code <aamandsahu2@gmail.com>
2026-02-25 17:59:22 +0530

    Update CreditsScreen.jsx

--- src/components/screens/CreditsScreen.jsx
+++ src/components/screens/CreditsScreen.jsx
@@ -5,7 +5,7 @@ import { RotateCcw } from "lucide-react"
 import Button from "../Button"
 
 const credits = [
-    { role: "The Birthday Queen", name: "ANSHIKA" },
+    { role: "The Birthday Princess", name: "ANSHIKA" },
     { role: "Graphic Artist", name: "KD" },
     { role: "Chief Happiness Officer", name: "You" },
     { role: "Music Curator", name: "KD" },
